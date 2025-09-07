756d26f13acf3d4af1533c28d3b7c9d5687d960b
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-07 12:47:17 +0900

    Use the Bottom Navigation Bar component in the main app file. The 4 main sections of the app can be accessed through the Bottom Navigation Bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,34 @@
-import { Route, Switch } from "wouter";
+import { Route, Switch, useLocation } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 
-// Pages
 import Landing from "./pages/landing";
 import Home from "./pages/home";
 // Later: import Challenges from "./pages/challenges";
+// Later: import Groups from "./pages/groups";
 // Later: import Profile from "./pages/profile";
 
+import BottomNavigation from "./components/common/BottomNavigation";
+
 function App() {
+    const [location] = useLocation();
+    const showBottomNav = ["/feed", "/challenges", "/groups", "/profile"].includes(location);
+
     return (
         <QueryClientProvider client={queryClient}>
-            <Switch>
-                {/* Landing page (default route) */}
-                <Route path="/" component={Landing} />
+            <div className="min-h-screen pb-16">
+                <Switch>
+                    <Route path="/" component={Landing} />
+                    <Route path="/feed" component={Home} />
 
-                {/* Feed page (mapped to /feed for consistency) */}
-                <Route path="/feed" component={Home} />
+                    {/* Future routes */}
+                    {/* <Route path="/challenges" component={Challenges} /> */}
+                    {/* <Route path="/groups" component={Groups} /> */}
+                    {/* <Route path="/profile" component={Profile} /> */}
+                </Switch>
 
-                {/* Future routes */}
-                {/* <Route path="/challenges" component={Challenges} /> */}
-                {/* <Route path="/profile" component={Profile} /> */}
-            </Switch>
+                {showBottomNav && <BottomNavigation />}
+            </div>
         </QueryClientProvider>
     );
 }
