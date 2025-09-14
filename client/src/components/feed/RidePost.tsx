2f1622019de61629da0d91be30e7350ddeb337c6
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-14 13:50:41 +0900

    RidePostProps is defining what props are needed for the component

--- client/src/components/feed/RidePost.tsx
+++ client/src/components/feed/RidePost.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Clock, MapPin } from "lucide-react";
+import { useAppStore } from "../../store/store";
 
-export default function RidePost() {
-    return <div>🚴 RidePost placeholder</div>;
+interface RidePostProps {
+    id: number;
+    user: string;
+    ride: string;
+    distance: string;
+    location: string;
+    time: string;
+    chapeauCount: number;
+    hasChapeaued: boolean;
+    comments: string[];
+}
+
+export default function RidePost({
+    id,
+    user,
+    ride,
+    distance,
+    location,
+    time,
+    chapeauCount,
+    hasChapeaued,
+    comments,
+}: RidePostProps) {
+
 }
